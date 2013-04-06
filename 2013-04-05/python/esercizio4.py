bf894b55c1be858f350d6445690bcf6bb1f10f96
Filippo89 <filippo.barbato@hotmail.it>
2013-04-06 18:10:00 +0200

    esercizio4.js

--- 2013-04-05/python/esercizio4.py
+++ 2013-04-05/python/esercizio4.py
@@ -2,7 +2,7 @@
 
 GRID = COMP([INSR(PROD),AA(QUOTE)])
 
-#east
+#east -> ripreso dall'esercizio precedente
 e1 = GRID([[-3.2-1.7,0.2+((0.5+4.7)*2)+0.5],[-0.5-3.6,0.5],[0.14+1,-1,0.36]])
 e2 = GRID([[-3.2-1.7,0.2+0.5+4.7,-0.5-4.7,0.5],[-0.5-3.6,0.5],[-0.14-1,1]])
 e3 = GRID([[10.9],[0.5],[-2.5,6.34]])
@@ -12,17 +12,13 @@ e6 = GRID([[-10.4,0.5,-4.7,5.7],[0.5],[-7.5,2.5]])
 e7 = GRID([[21.3],[0.5],[-10,0.5]])
 east = STRUCT([e1,e2,e3,e4,e5,e6,e7])
 
-finestra7 = COLOR(GREEN)(R([2,3])(PI/2)(CUBOID([10.4,1.17,0.25])))
+#aggiunte le finestre colorate
 
 finestra = COLOR(GREEN)(R([2,3])(PI/2)(CUBOID([4.7,1.17,0.25])))
 
-finestra_trasl = T([1,2,3])([10.9,3.95,1.30])(finestra)
-
 finestra1 = T([1,2,3])([10.9,0.25,2.5+1.36])(finestra)
-
 finestre = STRUCT(NN(3)([finestra1,T([3])(2.5)]))
-
-east_color = STRUCT([e1,e2,e3,e4,e5,e6,e7,finestra_trasl, finestre])
+east_color = STRUCT([e1,e2,e3,e4,e5,e6,e7,finestre])
 
 #prova di finestre colorate
 #VIEW(east_color)
--- 2013-04-05/python/esercizio5.py
+++ 2013-04-05/python/esercizio5.py
@@ -5,11 +5,11 @@ step2d = MKPOL([[[0,0],[0.54,0.36],[0.54,0.72],[0,0.72]],[[1,2,3,4]],None])
 
 
 #gradino singolo in 3 dimensioni
-step3d = MAP([S1,S3,S2])(PROD([step2d, Q(9)]))
+step3d = MAP([S1,S3,S2])(PROD([step2d, Q(2)]))
 
 #rampa di scale
-ramp = STRUCT(NN(13)([step3d, T([1,3])([0.54,0.54-0.18])]))
-
+ramp = STRUCT(NN(13/2)([step3d, T([1,3])([0.54,0.54-0.18])]))
+ramp1 = S([2])([-0.5])(ramp)
 #prova rampa
 #VIEW(ramp)
 
